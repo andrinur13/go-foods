e50c30fccb756b45ffb89cf987cde5f356244965
andrinur13 <andribis13@gmail.com>
2021-04-17 16:57:14 +0700

    add env features

--- main.go
+++ main.go
@@ -4,15 +4,35 @@ import (
 	"fmt"
 	"go-foods/foods"
 	"go-foods/handler"
+	"log"
+	"os"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	dsn := "andri:root@tcp(127.0.0.1:3306)/go_foods?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// get env
+	userName := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	hostIP := os.Getenv("HOST")
+	portServer := os.Getenv("PORTDB")
+	dbName := os.Getenv("DATABASE")
+
+	// read db
+	dsnString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", userName, password, hostIP, portServer, dbName)
+
+	// cadangan siapa tau gagal :v
+	// dsn := "andri:root@tcp(127.0.0.1:3306)/go_foods?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := dsnString
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
